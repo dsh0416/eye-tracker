47d7adc9e6a17523742a4d6770c5cafd0e1875cb
Delton Ding <dsh0416@gmail.com>
2023-05-01 11:31:50 +0800

    fix crash

--- facial_detection.py
+++ facial_detection.py
@@ -22,13 +22,14 @@ if __name__ == '__main__':
 
     # Preprocessing
     image = imutils.resize(frame, width=720)
+    display = image.copy()
     grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     rects = detector(grayscale, 1)
 
     for (i, rect) in enumerate(rects):
       # Facial border
       (x, y, w, h) = rect_to_borders(rect)
-      cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+      cv2.rectangle(display, (x, y), (x + w, y + h), (255, 255, 0), 2)
 
       # Facial landmark
       predicted = predictor(grayscale, rect)
@@ -42,8 +43,8 @@ if __name__ == '__main__':
       left_eye_center = left_eye.mean(axis=0).astype('int')
       right_eye = landmarks[42:48]
       right_eye_center = right_eye.mean(axis=0).astype('int')
-      cv2.circle(image, tuple(left_eye_center), 1, (0, 0, 255), -1)
-      cv2.circle(image, tuple(right_eye_center), 1, (0, 255, 0), -1)
+      cv2.circle(display, tuple(left_eye_center), 1, (0, 0, 255), -1)
+      cv2.circle(display, tuple(right_eye_center), 1, (0, 255, 0), -1)
 
 
       # Get 32x32 eye image
@@ -53,21 +54,23 @@ if __name__ == '__main__':
       eye_left_y = left_eye_center[1] - eye_height // 2
       eye_right_x = right_eye_center[0] - eye_width // 2
       eye_right_y = right_eye_center[1] - eye_height // 2
-      eye_left = grayscale[eye_left_y:eye_left_y + eye_height, eye_left_x:eye_left_x + eye_width]
-      eye_right = grayscale[eye_right_y:eye_right_y + eye_height, eye_right_x:eye_right_x + eye_width]
-      cv2.putText(image, 'L', (eye_left_x, eye_left_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-      cv2.putText(image, 'R', (eye_right_x, eye_right_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+      if eye_left_x < 0 or eye_left_y < 0 or eye_right_x < 0 or eye_right_y < 0:
+        continue
+      if eye_left_x + eye_width > image.shape[1] or eye_left_y + eye_height > image.shape[0] or eye_right_x + eye_width > image.shape[1] or eye_right_y + eye_height > image.shape[0]:
+        continue
+
+      eye_left = image[eye_left_y:eye_left_y + eye_height, eye_left_x:eye_left_x + eye_width]
+      eye_right = image[eye_right_y:eye_right_y + eye_height, eye_right_x:eye_right_x + eye_width]
+      cv2.putText(display, 'L', (eye_left_x, eye_left_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+      cv2.putText(display, 'R', (eye_right_x, eye_right_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
       # Show image
-      eye_window = np.zeros((eye_height, eye_width * 2), dtype='uint8')
+      eye_window = np.zeros((eye_height, eye_width * 2, 3), dtype='uint8')
       eye_window[0:eye_height, 0:eye_width] = eye_left
       eye_window[0:eye_height, eye_width:eye_width * 2] = eye_right
-      image[0:eye_height, 0:eye_width * 2, 0] = eye_window
-      image[0:eye_height, 0:eye_width * 2, 1] = eye_window
-      image[0:eye_height, 0:eye_width * 2, 2] = eye_window
+      display[0:eye_height, 0:eye_width * 2] = eye_window
 
-
-    cv2.imshow('Press \'Q\' to quit', image)
+    cv2.imshow('Press \'Q\' to quit', display)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
